6f205c82fa4257745270f2fe25a8ed2fd246d5f0
Jing ZHANG <jing.zhang.ferment@gmail.com>
2024-12-01 21:37:25 +0100

    change the error message for password

--- controllers/AdminController.php
+++ controllers/AdminController.php
@@ -130,7 +130,7 @@ class AdminController {
         // On vérifie que le mot de passe est correct.
         if (!password_verify($password, $user->getPassword())) {
             $hash = password_hash($password, PASSWORD_DEFAULT);
-            throw new Exception("Le mot de passe est incorrect : $hash");
+            throw new Exception("Le mot de passe est incorrect.");
         }
 
         // On connecte l'utilisateur.
